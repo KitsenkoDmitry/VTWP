fba8ebcdf19b880d1f69d00645d83d074ffa93f3
Dmitry Kitsenko <kitsenkodv@gmail.com>
2019-03-30 11:37:32 +0300

    checkout page

--- html/src/js/partials/tabs.js
+++ html/src/js/partials/tabs.js
@@ -8,9 +8,27 @@ $(document).on('click', '.js-tabs-link', (e) => {
     const $tabsLinks = $tabs.find('.js-tabs-link');
     const $tabsItems = $tabs.find('.js-tabs-item');
 
+    // выключаем все активные табы и ссылки
     $tabsLinks.removeClass('is-active');
     $tabsItems.removeClass('is-active');
 
+    // выключаем валидацию у скрытых полей и очищаем их
+    let $hiddenFormFields = $tabsItems.find('[data-required]');
+    if ($hiddenFormFields.length) {
+        $hiddenFormFields.prop('data-required', false);
+        $hiddenFormFields.prop('required', false);
+        $hiddenFormFields.val('');
+    }
+
+    // включаем нужный таб и делаем нужную ссылку активной
     $self.addClass('is-active');
-    $($self.attr('href')).addClass('is-active');
+    const $selfItem = $($self.attr('href'));
+    $selfItem.addClass('is-active');
+
+    // включаем валидацию у скрытых полей
+    $hiddenFormFields = $selfItem.find('[data-required]');
+    if ($hiddenFormFields.length) {
+        $hiddenFormFields.prop('data-required', true);
+        $hiddenFormFields.prop('required', true);
+    }
 });
